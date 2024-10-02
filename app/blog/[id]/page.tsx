af425ca384235cf6655e4e4781cdd9512f3b7cdf
wook <yho7955@naver.com>
2024-10-02 13:38:53 +0900

    feat: 블로그 라우터 이동되도록 추가

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+const posts = [
+  {
+    id: "1",
+    title: "First Post",
+    content:
+      "This is the content of the first post. It's about 400-500 words long and covers a specific topic in detail.",
+  },
+  {
+    id: "2",
+    title: "Second Post",
+    content:
+      "This is the content of the second post. It also meets the word count and includes insightful information for readers.",
+  },
+];
 
-export default function page() {
-  return <div>page</div>;
+export default function BlogPost(params: { id: string }) {
+  const { id } = params;
+  console.log(id);
+
+  return (
+    <div>
+      <h1>Blog Post</h1>
+      <p>Post content goes here</p>
+      <span>{id}</span>
+    </div>
+  );
 }
