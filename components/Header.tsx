5d2da57f7addf5c04daf065b0df4c1d4a6c53bfa
wook <yho7955@naver.com>
2024-10-02 15:38:22 +0900

    헤더 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import Link from "next/link";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="shadow-md border-b pb-2">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
+        <div className="flex justify-between items-center h-16 ">
+          <div className="flex-shrink-0 flex items-center">
+            <Link href="/" className="text-2xl font-bold text-white ">
+              쇼핑몰 블로그
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
