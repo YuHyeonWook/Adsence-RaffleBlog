88bc618420194b66b689aab0f63d7f2f3e658734
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-10-02 15:28:13 +0900

    style : 상세페이지 수정

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -8,15 +8,25 @@ export default function BlogPost() {
   const { id } = params;
 
   const post = posts.find((p) => p.id === id);
+  const paragraphs = post?.content.split("\n");
 
   if (!post) {
-    return <div>Post not found</div>;
+    return <div className="text-center text-red-500">Post not found</div>;
   }
 
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
+    <div className="flex flex-col items-center justify-center min-h-screen ">
+      <article className="prose lg:prose-xl max-w-4xl p-8 rounded-lg shadow-md">
+        <header className="border-b pb-4 mb-8">
+          <h1 className="text-4xl font-bold">{post.title}</h1>
+        </header>
+
+        <section className="space-y-6">
+          {paragraphs?.map((paragraph, index) => (
+            <p key={index}>{paragraph}</p>
+          ))}
+        </section>
+      </article>
     </div>
   );
 }
