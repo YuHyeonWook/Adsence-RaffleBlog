daac1b0bcceae70cb89efc93f3e589c5f7947684
wook <yho7955@naver.com>
2024-10-02 16:31:39 +0900

    feat: 애드센스 코드 스니펫 추추

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
+import { GoogleAdSense } from "@/components/GoogleAdSense";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Header />
         {children}
       </body>
+      <GoogleAdSense />
     </html>
   );
 }
